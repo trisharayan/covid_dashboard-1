ea7086b0a6ba4d08bba3eb3bb4e0889bf642c589
sandeepsn1997 <sandeepsn1997@gmail.com>
2020-04-18 01:55:42 +0530

    usa map

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ApiService } from '../api.service';
 import * as Highcharts from 'highcharts';
 
@@ -11,6 +11,7 @@ import * as Highcharts from 'highcharts';
 export class DashboardComponent implements OnInit {
 
   constructor(private service: ApiService) { }
+  @ViewChild('container', { static: true }) container: ElementRef
 
   count = {
     updated_at: "",
@@ -23,46 +24,43 @@ export class DashboardComponent implements OnInit {
 
   public options: any = {
     chart: {
-      type: 'scatter',
-      height: 700
+      type: 'line'
     },
     title: {
-      text: 'Sample Scatter Plot'
+      text: 'COVID-19 Count'
     },
-    credits: {
-      enabled: false
-    },
-    tooltip: {
-      formatter: function () {
-        return 'x: ' + Highcharts.dateFormat('%e %b %y %H:%M:%S', this.x) + ' y: ' + this.y.toFixed(2);
-      }
+    subtitle: {
+      text: ''
     },
     xAxis: {
-      type: 'datetime',
-      labels: {
-        formatter: function () {
-          return Highcharts.dateFormat('%e %b %y', this.value);
-        }
+      categories: []
+    },
+    yAxis: {
+      title: {
+        text: 'Count'
+      },
+      resize: {
+        enabled: true
+      }
+    },
+    plotOptions: {
+      line: {
+        dataLabels: {
+          enabled: true
+        },
+        enableMouseTracking: false
       }
     },
     series: [
-      {
-        name: 'Normal',
-        turboThreshold: 500000,
-        data: [[new Date('2018-01-25 18:38:31').getTime(), 2]]
-      },
-      {
-        name: 'Abnormal',
-        turboThreshold: 500000,
-        data: [[new Date('2018-02-05 18:38:31').getTime(), 7]]
-      }
     ]
   }
 
+
   ngOnInit() {
-    Highcharts.chart('container', this.options);
+
     this.getCovidCount()
     this.getWorldCount()
+
   }
 
   getCovidCount() {
@@ -73,14 +71,72 @@ export class DashboardComponent implements OnInit {
       this.count["confirmed"] = data[0]["confirmed"]
       this.count["recovered"] = data[0]["recovered"]
       this.count["active"] = data[0]["active"]
+
+      let xAxisData = []
+      let Graphdata = [{
+        name: 'Confirmed',
+        data: []
+      }, {
+        name: 'Active',
+        data: []
+      },
+      {
+        name: 'Deaths',
+        data: []
+      },
+      {
+        name: 'Recovered',
+        data: []
+      }
+      ]
+
+
+      data = data.reverse();
+      data.forEach(element => {
+        xAxisData.push(element.date)
+        Graphdata[0].data.push(element.confirmed)
+        Graphdata[1].data.push(element.active)
+        Graphdata[2].data.push(element.deaths)
+        Graphdata[3].data.push(element.recovered)
+
+
+      });
+
+      this.options.xAxis.categories = xAxisData
+      this.options.series = Graphdata
+
+
+      Highcharts.chart(this.container.nativeElement, this.options);
+
+
     })
+
   }
-  
+
   getWorldCount() {
     this.service.getWorldCount().subscribe((data) => {
       this.worldcounttable = data.data;
     })
   }
+
+
+  getTimeLine() {
+
+
+    this.service.getWorldCount().subscribe((data) => {
+      this.worldcounttable = data.data;
+    })
+
+
+  }
+
+
+
+
+
+
+
+
 }
 
 
